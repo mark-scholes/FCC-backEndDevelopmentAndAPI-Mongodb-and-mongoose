2a27a679128cca7c003dafc277b1c517b3412114
mark-scholes <innominatus15@outlook.com>
2022-10-15 14:59:08 +0100

    This exercise required us to create a personal Schema Prototype. Then create a Personal model using the personSchema protoype

--- myApp.js
+++ myApp.js
@@ -3,9 +3,18 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-console.log(mongoose.connection.readyState);
+//create a personal Schema Prototype
+let personSchema = new mongoose.Schema({
+  name : {
+    type: String,
+    required: true
+  },
+  age :  Number,
+  favoriteFoods : [String]
+})
 
-let Person;
+//create a Personal model using the personSchema protoype 
+let Person = mongoose.model('Person', personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
